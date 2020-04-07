45bcc3be3427ba355ee2247030dda225dad05239
radbrt <hho@cerx.co>
2020-04-07 23:25:02 +0200

    included small data and notes

---
+++ appendix/job_ads/README.md
@@ -0,0 +1,5 @@
+# You can't work from home
+
+Short attempt at estimating relative probability of working from home, based on mentions of "hjemmekontor" in job ads.
+
+Naturally, not all ads mention this as a perk, but in lieu of better information we'll stipulate that for a given occupation with WFH possibility, there is a given probability that a random employer will mention it as a perk.
---
+++ appendix/job_ads/jobs.R
@@ -0,0 +1,38 @@
+library(tidyverse)
+
+jobs <- read_delim('Data/nav_open/stillinger_all_merged.csv', ";", col_types = cols(.default=col_character()))
+
+max(jobs$statistikk_aar_mnd)
+
+max(substr(jobs$registrert_dato, 7, 10))
+
+library(arrow)
+
+jobs %>% 
+  filter(isco_versjon=='ISCO-08') %>% 
+  write_parquet("stillinger_all_merged_isco08.parquet")
+
+library(stringr)
+
+
+jobs %>% 
+  mutate(hk = grepl("hjemme", stillingsbeskrivelse_vasket, ignore.case=T )) %>% 
+  select(isco_versjon, yrkeskode, yrke, hk, arbeidssted_kommunenummer, statistikk_aar_mnd) -> hjemme
+
+hjemmekontor %>% 
+  filter(statistikk_aar_mnd>'2017') %>% 
+  mutate(st1 = substr(yrkeskode,1 ,1)) %>% 
+  group_by(st1) %>% 
+  summarize(hk_prob = mean(hk)) -> hk_yrke_prob
+
+hk_ant = sum(hjemmekontor[hjemmekontor$statistikk_aar_mnd>'2017', c('hk')]$hk)
+
+nrow(hjemmekontor[hjemmekontor$statistikk_aar_mnd>'2017', c('hk')])
+
+hjemmekontor %>% 
+  filter(statistikk_aar_mnd>'2017') %>%
+  group_by(yrkeskode, yrke) %>% 
+  summarize(hk_prob = mean(hk), hk_antall = sum(hk), antall = n(), rel_prob = sum(hk)/hk_ant ) -> hk_st4_prob_tekst
+
+write_delim(hk_st4_prob_tekst, "R/jobs/yrke_hjemmekontor_prob_yrkestekst.csv", delim = ";")
+write_delim(hk_yrke_prob, "R/jobs/yrke_hjemmekontor_prob_styrk1.csv", delim = ";")
